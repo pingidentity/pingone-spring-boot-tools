87bfca36bbc735e6421b6bf9d3577298c985e754
Ivan Denysenko <ivandenysenko@pingidentity.com>
2020-03-03 15:05:20 +0200

    P14C-8227: removed a programmatic SSL configuration

--- src/main/java/com/pingidentity/spring/example/config/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/pingidentity/spring/example/config/OAuth2LoginSecurityConfig.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
@@ -14,6 +15,7 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequ
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@EnableWebSecurity
 public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
@@ -25,11 +27,6 @@ public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
     http.authorizeRequests()
         .antMatchers("/login", "/js/**", "/css/**", "/img/**", "/webjars/**").permitAll()
         .anyRequest().authenticated()
-
-        // Enable ssl
-        .and().requiresChannel()
-        .anyRequest()
-        .requiresSecure()
 
         // Handling Login
         .and().oauth2Login()
