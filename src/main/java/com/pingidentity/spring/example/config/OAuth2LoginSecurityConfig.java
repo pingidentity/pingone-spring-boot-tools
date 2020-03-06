ed5cc83fd8eff7a034f21934ca54fa0808b7ec11
Ivan Denysenko <ivandenysenko@pingidentity.com>
2020-03-06 13:31:27 +0200

    P14C-8227: @EnableWebSecurity has been deleted because it is not needed

--- src/main/java/com/pingidentity/spring/example/config/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/pingidentity/spring/example/config/OAuth2LoginSecurityConfig.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
@@ -15,7 +14,6 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequ
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-@EnableWebSecurity
 public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
